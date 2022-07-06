eecd42748814f1d8a4e8be7111691510279162c1
Nikita Egorov <n1ck.egorov@yandex.ru>
2022-07-06 16:32:24 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'spicy_dancer',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
